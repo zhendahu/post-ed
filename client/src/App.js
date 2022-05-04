3c7d735dd93abf06c445f96deec0595c26875ab4
Kunal Patil <patil.kunal919@gmail.com>
2022-05-03 17:28:16 -0700

    Implement TaskGroup component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TaskGroup from './components/TaskGroup'
+import {CardGroup} from 'react-bootstrap'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CardGroup>
+    <TaskGroup tasks={["Task 1", "Task 2", "Task 3"]} subtitle="Work in progress to be completed" title = "To-Do"></TaskGroup>
+    <TaskGroup tasks={["Task 1", "Task 2", "Task 3"]} subtitle="Work in progress currently on-hold" title = "On-Hold"></TaskGroup>
+    <TaskGroup tasks={["Task 1", "Task 2", "Task 3"]} subtitle="Work that has been completed" title = "Completed"></TaskGroup>
+    </CardGroup>
+  )
 }
 
 export default App;
---
+++ client/src/components/TaskGroup.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {ListGroup, Button, Card, ToggleButton} from 'react-bootstrap'
+import TrashBin from '../static/images/trashbin.png'
+
+
+
+export default class TaskGroup extends Component{
+
+
+    addItem(){
+        console.log("Hello World!")
+    }
+
+    removeItem(){
+        console.log("Goodbye World!")
+    }
+
+    render(){
+
+        return(
+            <Card className="text-center">
+            <Card.Body>
+                <Card.Title>{this.props.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{this.props.subtitle}</Card.Subtitle>
+            <ListGroup className="list-group-flush" bg='dark'>
+            {this.props.tasks.map(task => 
+           
+            <ListGroup.Item>
+                <big>{task}</big>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <Button variant='outline-danger' size="sm" onClick={() => this.removeItem()} >   
+                <img src={TrashBin} alt="add item" width="10" /> 
+                </Button>
+            </ListGroup.Item>
+   
+            )
+            }
+            </ListGroup>
+            <br></br>
+            <ToggleButton onClick={() => this.addItem()} variant='success' > + New Task </ToggleButton>
+            </Card.Body>
+            </Card>
+     
+        )
+
+    }
+
+
+}
