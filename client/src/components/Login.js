0879f206844b4300bc792b00b8272ad1304c2062
bulesky1234 <3438889665@qq.com>
2022-05-04 22:12:46 -0700

    minrui/user login UI

---
+++ client/src/components/Login.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./Login.css"
+
+class LoginModal extends React.Component {
+  render() {
+    return (
+      <div id="form">
+        <Form style={{margin:"1rem"}}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>User Name</Form.Label>
+            <Form.Control type="email" placeholder="Enter username" />
+
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default LoginModal;
