965a0a39496976ab74e81281aa456072658cf7ad
Kunal Patil <patil.kunal919@gmail.com>
2022-05-04 23:27:41 -0700

    Fix conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,50 @@
+import React from "react";
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import TaskGroup from './components/TaskGroup'
-import {CardGroup} from 'react-bootstrap'
-function App() {
+import { Button } from "react-bootstrap";
+import {TaskModal} from "./components/TaskModal.js"
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      show:true
+    }
+  }
+
+  handleClose(){
+    this.setState({
+      show:false
+    })
+  }
+  render(){
+  
   return (
-    <CardGroup>
-    <TaskGroup tasks={["Task 1", "Task 2", "Task 3"]} subtitle="Work in progress to be completed" title = "To-Do"></TaskGroup>
-    <TaskGroup tasks={["Task 1", "Task 2", "Task 3"]} subtitle="Work in progress currently on-hold" title = "On-Hold"></TaskGroup>
-    <TaskGroup tasks={["Task 1", "Task 2", "Task 3"]} subtitle="Work that has been completed" title = "Completed"></TaskGroup>
-    </CardGroup>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button variant="primary" onClick={()=>this.setState({show:true})}>
+        Launch demo modal
+      </Button>
+      <TaskModal show={this.state.show} onHide={()=>this.handleClose()}></TaskModal>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  }
 }
 
 export default App;
