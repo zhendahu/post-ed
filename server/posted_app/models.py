b95ccc36e350943ae16236f08c3993f27d819ec4
zhendahu <zhendahu@gmail.com>
2022-05-06 21:46:19 -0700

    UserManager, User, Task, Team Models
    Created UserManager, User, Task, and Team models, still have empty TaskGroup and TaskModal models

--- server/posted_app/models.py
+++ server/posted_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class UserManager(models.Manager):
+    def basic_validator(self, post_data):
+        errors = {}
+        try:
+            if len(post_data["username_input"]) > 25:
+                errors["username_input"] = "Username should be 25 characters long or less."
+            elif len(post_data["username_input"]) < 2:
+                errors["username_input"] = "Username should be at least 2 characters long."
+            if len(post_data["password_input"]) < 8:
+                errors["password_input"] = "Password should be longer than 8 characters long."
+        except:
+            errors["unknown_error"] = "An unknown error has occured."
+        return errors
+
+class User(models.Model):
+    objects = UserManager()
+    username = models.CharField(max_length=25)
+    password = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=25)
+    team_users = models.ManyToManyField(User, related_name="joined_teams")
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=50)
+    task_description = models.CharField(max_length=250)
+    author = models.ForeignKey(User, related_name="post_author", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class TaskGroup(models.Model):
+
+
+class TaskModal(models.Model):
