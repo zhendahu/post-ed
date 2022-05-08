24d2eefc394e5850c13ef1a9f7a47eef31805b5d
lissa-c <104601406+lissa-c@users.noreply.github.com>
2022-05-08 00:00:08 -0700

    Create Task.js
    Replacement for old Task.js since it was renamed to CreateTask.js

---
+++ client/src/components/Task.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+//task component containing information acquired from CreateTask
+//includes checkbox functionality for user to mark completed tasks
+//and uncheck box in event task was not sufficiently completed
