374a9e6c11faa3b0bed411ad37db3a00a471318e
Anakin Trotter <anakin.trotter@gmail.com>
2022-05-28 21:55:18 -0700

    Patched up profile page

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Form, Button, ListGroup } from "react-bootstrap";
+import { Button, ListGroup, Image } from "react-bootstrap";
 import { useParams } from "react-router";
-import App from "../App";
 import './UserProfile.css';
 
 
@@ -16,35 +15,46 @@ function UserProfile(props) {
             });
     }, []);
 
+    if (userInfo.groups && userInfo.groups.length === 0) {
+        userInfo.groups = "None"
+    } else {
+        // TODO
+    }
+
     return (
         <div className="profile-background">
-            <div id="header1">
-                <h1>Post-Ed</h1>
-            </div>
-            <div id="header2">
-                <h2>{userInfo.username}'s Profile</h2>
-            </div>
+            <h1>Post-Ed</h1>
+            <h2>{userInfo.username}'s Profile</h2>
 
+            {/* <div id="profile-picture">
+                <img id="profpic" src={props.profilepicture} width='300px' />
+            </div> */}
+
+            <Image src="/logo192.png"></Image>
+
+            <div>
+                <Button>Edit profile</Button>
+                <Button>Create new group</Button>
+                <Button>Join group</Button>
+            </div>
+            <br></br>
             <div id="listgroup">
                 <ListGroup.Item>Name: {userInfo.username} </ListGroup.Item>
                 <ListGroup.Item>Email: {userInfo.email} </ListGroup.Item>
                 <ListGroup.Item>Groups: {userInfo.groups} </ListGroup.Item>
-                <ListGroup.Item>Assigned tasks: {props.tasks} </ListGroup.Item>
+                {/* <ListGroup.Item>Assigned tasks: {props.tasks} </ListGroup.Item> */}
             </div>
 
-            <div id="profile-picture">
-                <img id="profpic" src={props.profilepicture} width='300px' />
-            </div>
 
-            <div id="edit-profile-button">
-                <button>Edit profile</button>
+            {/* <div id="edit-profile-button">
+                <Button>Edit profile</Button>
             </div>
             <div id="create-group">
-                <button>Create new group</button>
+                <Button>Create new group</Button>
             </div>
             <div id="join-group">
-                <button>Join group</button>
-            </div>
+                <Button>Join group</Button>
+            </div> */}
         </div>
     );
 
